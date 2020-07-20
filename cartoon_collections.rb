dc2e5304e8bdac3e1df0415589caf39a90b76aa1
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-20 00:23:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,4 @@ def find_the_cheese(array)
   array.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end
